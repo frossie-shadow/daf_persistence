7f952646197c32287ec76ceb86d8d6b3b04aac91
Nathan Pease <npease@slac.stanford.edu>
2017-08-04 17:32:50 -0700

    convert fmtPosixRepositoryCfg from a class to callables

--- python/lsst/daf/persistence/fmtPosixRepositoryCfg.py
+++ python/lsst/daf/persistence/fmtPosixRepositoryCfg.py
@@ -22,100 +22,95 @@
 
 import copy
 import errno
-import fcntl
 import yaml
 import os
 import urllib
 from . import PosixStorage, RepositoryCfg, safeFileIo, ParentsMismatch
-from lsst.log import Log
 
 
-class RepositoryCfgPosixFormatter():
+def write(butlerLocation, cfg):
+    """Serialize a RepositoryCfg to a location.
 
-    @classmethod
-    def write(cls, cfg, butlerLocation):
-        """Serialize a RepositoryCfg to a location.
+    When the location is the same as cfg.root, the RepositoryCfg is to be written at the root location of
+    the repository. In that case, root is not written in the serialized cfg; it is implicit in the
+    location of the cfg. This allows the cfg to move from machine to machine without modification.
 
-        When the location is the same as cfg.root, the RepositoryCfg is to be written at the root location of
-        the repository. In that case, root is not written in the serialized cfg; it is implicit in the
-        location of the cfg. This allows the cfg to move from machine to machine without modification.
+    Parameters
+    ----------
+    cfg : RepositoryCfg instance
+        The RepositoryCfg to be serialized.
+    butlerLocation : ButlerLocation
+        The location to write the RepositoryCfg.
+    """
+    def setRoot(cfg, loc):
+        loc = os.path.split(loc)[0]  # remove the `repoistoryCfg.yaml` file name
+        if loc is None or cfg.root == loc:
+            cfg = copy.copy(cfg)
+            loc = cfg.root
+            cfg.root = None
+        return cfg
 
-        Parameters
-        ----------
-        cfg : RepositoryCfg instance
-            The RepositoryCfg to be serialized.
-        butlerLocation : ButlerLocation
-            The location to write the RepositoryCfg.
-        """
-        def setRoot(cfg, loc):
-            loc = os.path.split(loc)[0]  # remove the `repoistoryCfg.yaml` file name
-            if loc is None or cfg.root == loc:
-                cfg = copy.copy(cfg)
-                loc = cfg.root
-                cfg.root = None
-            return cfg
-
-        # This class supports schema 'file' and also treats no schema as 'file'.
-        # Split the URI and take only the path; remove the schema from loc if it's there.
-        loc = butlerLocation.storage.root
-        parseRes = urllib.parse.urlparse(loc if loc is not None else cfg.root)
-        loc = os.path.join(parseRes.path, butlerLocation.getLocations()[0])
-        try:
-            with safeFileIo.SafeLockedFileForRead(loc) as f:
-                existingCfg = RepositoryCfgPosixFormatter._read(f, parseRes.path)
-                if existingCfg == cfg:
-                    cfg.dirty = False
-                    return
-        except IOError as e:
-            if e.errno != errno.ENOENT:  # ENOENT is 'No such file or directory'
-                raise e
-        with safeFileIo.SafeLockedFileForWrite(loc) as f:
-            existingCfg = RepositoryCfgPosixFormatter._read(f, parseRes.path)
-            if existingCfg is None:
-                cfgToWrite = setRoot(cfg, loc)
-            else:
-                if existingCfg == cfg:
-                    cfg.dirty = False
-                    return
-                try:
-                    existingCfg.extend(cfg)
-                    cfgToWrite = setRoot(existingCfg, loc)
-                except ParentsMismatch as e:
-                    raise RuntimeError("Can not extend existing repository cfg because: {}".format(e))
-            yaml.dump(cfgToWrite, f)
-            cfg.dirty = False
-
-    @classmethod
-    def _read(cls, fileObject, uri):
-        """Get a persisted RepositoryCfg from an open file object.
-
-        Parameters
-        ----------
-        fileObject : an open file object
-            the file that contains the RepositoryCfg.
-
-        Returns
-        -------
-        A RepositoryCfg instance or None
-        """
-        repositoryCfg = yaml.load(fileObject)
-        if repositoryCfg is not None:
-            if repositoryCfg.root is None:
-                repositoryCfg.root = uri
-        return repositoryCfg
-
-    @classmethod
-    def read(cls, butlerLocation):
-        repositoryCfg = None
-        loc = butlerLocation.storage.root
-        fileLoc = os.path.join(loc, butlerLocation.getLocations()[0])
-        try:
-            with safeFileIo.SafeLockedFileForRead(fileLoc) as f:
-                repositoryCfg = RepositoryCfgPosixFormatter._read(f, loc)
-        except IOError as e:
-            if e.errno != errno.ENOENT:  # ENOENT is 'No such file or directory'
-                raise e
-        return repositoryCfg
+    # This class supports schema 'file' and also treats no schema as 'file'.
+    # Split the URI and take only the path; remove the schema from loc if it's there.
+    loc = butlerLocation.storage.root
+    parseRes = urllib.parse.urlparse(loc if loc is not None else cfg.root)
+    loc = os.path.join(parseRes.path, butlerLocation.getLocations()[0])
+    try:
+        with safeFileIo.SafeLockedFileForRead(loc) as f:
+            existingCfg = _read(f, parseRes.path)
+            if existingCfg == cfg:
+                cfg.dirty = False
+                return
+    except IOError as e:
+        if e.errno != errno.ENOENT:  # ENOENT is 'No such file or directory'
+            raise e
+    with safeFileIo.SafeLockedFileForWrite(loc) as f:
+        existingCfg = _read(f, parseRes.path)
+        if existingCfg is None:
+            cfgToWrite = setRoot(cfg, loc)
+        else:
+            if existingCfg == cfg:
+                cfg.dirty = False
+                return
+            try:
+                existingCfg.extend(cfg)
+                cfgToWrite = setRoot(existingCfg, loc)
+            except ParentsMismatch as e:
+                raise RuntimeError("Can not extend existing repository cfg because: {}".format(e))
+        yaml.dump(cfgToWrite, f)
+        cfg.dirty = False
 
 
-PosixStorage.registerFormatter(RepositoryCfg, RepositoryCfgPosixFormatter)
+def _read(fileObject, uri):
+    """Get a persisted RepositoryCfg from an open file object.
+
+    Parameters
+    ----------
+    fileObject : an open file object
+        the file that contains the RepositoryCfg.
+
+    Returns
+    -------
+    A RepositoryCfg instance or None
+    """
+    repositoryCfg = yaml.load(fileObject)
+    if repositoryCfg is not None:
+        if repositoryCfg.root is None:
+            repositoryCfg.root = uri
+    return repositoryCfg
+
+
+def read(butlerLocation):
+    repositoryCfg = None
+    loc = butlerLocation.storage.root
+    fileLoc = os.path.join(loc, butlerLocation.getLocations()[0])
+    try:
+        with safeFileIo.SafeLockedFileForRead(fileLoc) as f:
+            repositoryCfg = _read(f, loc)
+    except IOError as e:
+        if e.errno != errno.ENOENT:  # ENOENT is 'No such file or directory'
+            raise e
+    return repositoryCfg
+
+
+PosixStorage.registerFormatters(RepositoryCfg, read, write)
